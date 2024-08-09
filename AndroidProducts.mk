1a890cb6d1b36faa7cc87078ff89ad6b415dc720
1xtAsh <ashraykrsingh321@gmail.com>
2024-08-09 23:00:38 +0530

    haydn: PixelOS-Ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,5 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_haydn.mk
+    $(LOCAL_DIR)/aosp_haydn.mk
 
+COMMON_LUNCH_CHOICES := \
+    aosp_haydn-user \
+    aosp_haydn-userdebug \
+    aosp_haydn-eng
